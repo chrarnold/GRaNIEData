576018ef58bac18ccb52e8d98b0aa764fe01e791
Christian Arnold <christian.arnold@embl.de>
2022-03-04 12:32:34 +0100

    Preparing Bioc submission

---
+++ R/GRNData.R
@@ -0,0 +1,33 @@
+#' GRaNIEData: Additional and more complex example data for the GRaNIE package
+#' 
+#' This companion package for \code{GRaNIE} provides some example datasets of a larger size than allowed for the \code{GRaNIE} package. 
+#' They include full and real-world examples for performing analyses with \code{GRaNIE}.
+#' 
+#' For a list of files that the package provides, type
+#' 
+#' \code{list.files(pattern = "*",system.file("extdata", package = "GRaNIEData"), full.names = TRUE)}
+#' 
+#' See also the [\code{GRaNIE} website with vignettes, code documentation etc](https://grp-zaugg.embl-community.io/GRaNIE).
+
+
+
+#' @section Data:
+#' For a description, see the corresponding package vignette by typing \code{browseVignettes("GRaNIEData")}.
+#' 
+#' @section Usage:
+#' You may use the example files to run a full \code{GRaNIE} analysis. For a full example, see the [workflow 
+#' vignette in the \code{GRaNIE} package](https://grp-zaugg.embl-community.io/GRaNIE/articles/workflow.html).
+
+#' 
+#' @section Contact Information: 
+#' 
+#' We value all the feedback that we receive and will try to reply in a timely manner.
+#' Please report any bug that you encounter as well as any feature request that you may have to us. 
+#' See the [\code{GRaNIE} website for how to contact us](https://grp-zaugg.embl-community.io/GRaNIE)
+
+#' 
+#' @docType package
+#' @keywords GRaNIEData, GRaNIE-Data, GRaNIEData-package
+#' @name GRaNIEData
+
+NULL
